11703b9097a303619035de7f98f562f5168104a8
Graham Pegg (MacBook) <graham@citrusmedia.com>
2016-12-17 12:29:48 -0500

    Now this actually does something

--- src/Snep.vue
+++ src/Snep.vue
@@ -3,8 +3,8 @@
 		<div class="trigger" @click="show">
 			<slot></slot>
 		</div>
-		<div :class="'box' + (shown ? ' shown' : ' hidden')">
-			<div :style="image"></div>
+		<div :class="'box-container' + (shown ? ' shown' : ' hidden')" @click="hide">
+			<div :style="background"></div>
 		</div>
 	</div>
 </template>
@@ -17,6 +17,11 @@
 				shown: false
 			}
 		},
+		computed: {
+			background () {
+				return 'background-image: url(' + this.image + ');';
+			}
+		},
 		methods: {
 			show () {
 				this.shown = true;
@@ -24,14 +29,48 @@
 			hide () {
 				this.shown = false;
 			}
+		},
+		mounted () {
+			// Preloader
+			(new Image()).src = this.image;
 		}
 	}
 </script>
 
-<style lang="sass">
-	.box {
-		//omg the lag is so aggravating, I gotta get some sleep, it's getting late :(
-		&.hidden { display: none;  }
-		&.shown  { display: block; }
+<style lang="scss">
+	.snep {
+		position: relative;
+		display: inline-block;
+		cursor: pointer;
+
+		.box-container {
+			position: fixed;
+			top: 0;
+			left: 0;
+			width: 100vw;
+			height: 100vh;
+			background: rgba(20, 20, 20, 0.75);
+			z-index: 9999999;
+
+			>div {
+				width: 80vw;
+				height: 80vh;
+				background-size: contain;
+				background-repeat: no-repeat;
+				background-position: center center;
+				position: absolute;
+				left: 50%;
+				top: 50%;
+				transform: translate(-50%, -50%);
+			}
+
+			&.hidden { display: none;  }
+			&.shown  { display: block; }
+		}
+
+		.trigger {
+			height: 100%;
+			width: 100%;
+		}
 	}
 </style>
--- src/entry.js
+++ src/entry.js
@@ -2,6 +2,7 @@ var Vue = require('vue');
 var Snep = require('./Snep.vue');
 
 var vm = new Vue({
+	el: ".wrapper",
 	components: {
 		Snep
 	}
